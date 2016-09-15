7b18c84ef4cc5b5a94455b3dfcf6f8c7e0a9c14a
unknown <mort5161@MININT-1I7MQ6O.esri.com>
2016-09-14 22:20:48 -0700

    Added some test projects

---
+++ src/TestClassLibrary.net45/Classes.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary.net45
+{
+    /// <summary>Include</summary>
+    /// <remarks>Exclude</remarks>
+    public interface PublicInterface
+    {
+        /// <summary>Include</summary>
+        /// <remarks>Exclude</remarks>
+        int MyProperty { get; }
+    }
+
+    /// <summary>Exclude</summary>
+    /// <remarks>Exclude</remarks>
+    internal interface InternalInterface
+    {
+        /// <summary>Include</summary>
+        /// <remarks>Exclude</remarks>
+        int MyProperty { get; }
+    }
+
+    /// <summary>Exclude</summary>
+    /// <remarks>Exclude</remarks>
+    internal class InternalClass
+    {
+        /// <summary>Exclude</summary>
+        /// <remarks>Exclude</remarks>
+        public void PublicMethod() { }
+
+        /// <summary>Exclude</summary>
+        /// <remarks>Exclude</remarks>
+        public int PublicProperty { get { return 0; } }
+    }
+
+    /// <summary>Include</summary>
+    /// <remarks>Exclude</remarks>
+    public class Class1
+    {
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        public class NestedPublicClass
+        {
+        }
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        internal class NestedInternalClass
+        {
+        }
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        private class NestedPrivateClass
+        {
+        }
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        protected void ProtectedMethod()
+        {
+        }
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        protected internal void ProtectedInternal()
+        {
+        }
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        /// <value>Include</value>
+        public int PublicProperty { get; } = 0;
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        /// <param name="parameter">Include</param>
+        /// <returns>Include</returns>
+        public int PublicMethod(int parameter) {  return 0; }
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        public event EventHandler PublicEvent;
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        public int PublicField;
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        /// <value>Include</value>
+        internal int InternalProperty { get; } = 0;
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        /// <param name="parameter">Exclude</param>
+        /// <returns>exclude</returns>
+        internal int InternalMethod(int parameter) { return 0; }
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        internal event EventHandler InternalEvent;
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        internal int InternalField;
+
+        /// <summary>Include</summary><remarks>exclude</remarks>
+        /// <value>Include</value>
+        private int PrivateProperty { get; } = 0;
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        /// <param name="parameter">Exclude</param>
+        /// <returns>exclude</returns>
+        private int PrivateMethod(int parameter) { return 0; }
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        private event EventHandler PrivateEvent;
+
+        /// <summary>Exclude</summary><remarks>exclude</remarks>
+        private int PrivateField;
+    }
+}
---
+++ src/XmlDocStripTaskTests/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmlDocStripTaskTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod_Net45()
+        {
+            var txt = System.IO.File.ReadAllText(@"..\..\..\TestClassLibrary.net45\bin\TestClassLibrary.net45.intellisense.XML");
+            Assert.IsFalse(txt.Contains("Exclude"));
+        }
+
+        [TestMethod]
+        public void TestMethod_UWP()
+        {
+            var txt = System.IO.File.ReadAllText(@"..\..\..\TestClassLibrary.UWP\bin\TestClassLibrary.UWP.intellisense.XML");
+            Assert.IsFalse(txt.Contains("Exclude"));
+        }
+    }
+}
